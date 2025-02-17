879a6166a0d09860a6bd8db46500c3fdf3d0ac48
Carlos Eduardo Martins Saldanha <165321087+Carlos-Bered@users.noreply.github.com>
2025-02-17 14:00:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -108,8 +108,8 @@ fechar.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
-// Fechar o modal ao clicar fora dele
-modal.addEventListener("click", (e) => {
+// Fechar o modal ao clicar fora dele (novo evento adicionado)
+window.addEventListener("click", (e) => {
     if (e.target === modal) {
         modal.style.display = "none";
     }
